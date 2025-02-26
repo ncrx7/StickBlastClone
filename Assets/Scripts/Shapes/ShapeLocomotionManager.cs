0df6eb3d72b1742d00a206e1aef0bb7b99e84b49
Batuhan Uysal <buysal31@gmail.com>
2025-02-26 21:18:18 +0300

    feat: Added move funcionality to shape by using pointers

---
+++ Assets/Scripts/Shapes/ShapeLocomotionManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Shapes
+{
+    public class ShapeLocomotionManager : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+    {
+        #region References
+        [SerializeField] private ShapeManager _shapeManager;
+        [SerializeField] private Transform _parentTransform;
+        #endregion
+
+        #region Variable
+        private Vector3 _offset;
+        private Vector3 _homePosition;
+        #endregion
+
+        #region MonoBeheviour Callbacks
+        private void Start()
+        {
+            _homePosition = _parentTransform.position;
+        }
+        #endregion
+
+        #region Interface Methods
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            SetOffset(eventData);
+        }
+        public void OnDrag(PointerEventData eventData)
+        {
+            Move(eventData);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+        private void SetOffset(PointerEventData eventData)
+        {
+            var target = Camera.main.ScreenToWorldPoint(eventData.position);
+            _offset = _parentTransform.position - target;
+        }
+
+        private void Move(PointerEventData eventData)
+        {
+            var target = Camera.main.ScreenToWorldPoint(eventData.position);
+            target += _offset;
+            target.z = 0;
+            _parentTransform.position = target;
+        }
+        #endregion
+    }
+}
