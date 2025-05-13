a79a7fc2c1275b3c2791d617106cad61fa822821
Batuhan Uysal <buysal31@gmail.com>
2025-05-13 13:33:23 +0300

    feat: Added rect transform rotation extension

--- Assets/Scripts/UI/Panels/HomePanel.cs
+++ Assets/Scripts/UI/Panels/HomePanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Data.Model;
 using DG.Tweening;
 using Enums;
+using Extensions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,16 +13,30 @@ namespace UI.MainMenu.Panels
 {
     public class HomePanel : BasePanel<MainPanelType, GameData>
     {
+        [Header("Buttons")]
         [SerializeField] private Button _startGameButton;
 
+        [Header("Quest")]
         [SerializeField] private Image _questSlider;
         [SerializeField] private TextMeshProUGUI _questText;
 
+        [Header("Gift")]
         [SerializeField] private Image _giftSlider;
 
+        [Header("Tweens")]
         Tween _questSliderTween, _giftSliderTween;
 
+        [Header("Image References")]
+        [SerializeField] private RectTransform _collectionButtonIcon;
+        
+        [Header("Settings")]
         [SerializeField] private float _sliderAnimationDuration;
+        [SerializeField] private float _collectionIconRotationSpeed;
+
+        private void Start()
+        {
+            _collectionButtonIcon.RotateYConstantly(_collectionIconRotationSpeed);
+        }
 
         public override void OnOpenPanel(GameData gameData)
         {
@@ -42,7 +57,7 @@ namespace UI.MainMenu.Panels
 
         private void AnimateSlider(GameData gameData, Image sliderLine, ref Tween tween)
         {
-            if(tween != null)
+            if (tween != null)
                 tween.Kill();
 
             sliderLine.fillAmount = 0;
--- Assets/Scripts/UtilsUnityCSharp/Extensions/RectTransformExtensions.cs
+++ Assets/Scripts/UtilsUnityCSharp/Extensions/RectTransformExtensions.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class RectTransformExtensions : MonoBehaviour
+namespace Extensions
 {
-    // Start is called before the first frame update
-    void Start()
+    public static class RectTransformExtensions
     {
-        
-    }
+        public static void RotateYConstantly(this RectTransform rectTransform, float degreesPerSecond)
+        {
+            float fullRotation = 360f;
+            float duration = fullRotation / degreesPerSecond;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            rectTransform.DORotate(new Vector3(0f, fullRotation, 0f), duration, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart);
+        }
     }
 }
